0f656e4a12f0836c9841f4a068c32ffbf8449f1d
wuguai <chounong@126.com>
2020-11-09 15:40:32 +0800

    config server and config client test

--- foxc-config-server/src/main/java/com/foxc/config/FoxcConfigServerApplication.java
+++ foxc-config-server/src/main/java/com/foxc/config/FoxcConfigServerApplication.java
@@ -1,4 +1,20 @@
 package com.foxc.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @SpringCloudApplication
+ * 包含 @SpringBootApplication、@EnableDiscoveryClient、@EnableCircuitBreaker，
+ * 分别是SpringBoot注解、注册服务中心Eureka注解、断路器注解
+ */
+@SpringCloudApplication
+@EnableConfigServer
 public class FoxcConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxcConfigServerApplication.class, args);
+    }
+
 }
--- foxc-provider-user/src/main/java/com/foxc/user/controller/TestConfigController.java
+++ foxc-provider-user/src/main/java/com/foxc/user/controller/TestConfigController.java
@@ -1,4 +1,24 @@
 package com.foxc.user.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @RereshScope - 每一次调用调用/refresh方法都会清除所有该类型的bean。下次使用时，就会重新创建，然后注入最新属性变量。
+ * @author 41990
+ */
+@RestController
+@RefreshScope
 public class TestConfigController {
+
+    @Value("${from}")
+    private String from;
+
+    @GetMapping("/from")
+    public String from() {
+        return this.from;
+    }
+
 }
